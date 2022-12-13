44421f7a9f7b998e64fdb34a7d50826fa428df12
sanjayjogi <s.jogi@somaiya.edu>
2022-12-13 11:22:53 +0530

    Push all

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'b52f147d79msh1ba40289dcc1d6fp1af8d7jsn0dc214813e0b',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "2dc71290f6040839fa3272d553e1ed39";
